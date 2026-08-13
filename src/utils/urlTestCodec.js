5a47dd4699cf4519b4c5b35085a28f28f451baa4
jgomez45-creator <jgomez45@us.es>
2026-08-13 19:57:24 +0200

    Fix: Integración de compresión LZ-String ultra-compacta. URLs de test directas reducidas a 2 líneas cortas y 100% autosuficientes

--- src/utils/urlTestCodec.js
+++ src/utils/urlTestCodec.js
@@ -1,42 +1,405 @@
 /**
- * Compresión y Decompresión Ultra Compacta para Enlaces Directos Ejecutables de Examen
- * Utiliza codificación UTF-8 Base64 pura (sin secuencias %20 ni %7B) para producir
- * enlaces extremadamente limpios y cortos de 1 sola línea.
+ * Compresión y Decompresión Ultra-Compacta LZ-String para Enlaces Directos Ejecutables de Examen
+ * 100% Autosuficiente: no requiere bases de datos externas, permisos de nube ni inicios de sesión.
  */
 
-function utf8ToBase64(str) {
-  try {
-    return btoa(
-      encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (_, p1) =>
-        String.fromCharCode(parseInt(p1, 16))
-      )
-    )
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_')
-      .replace(/=+$/, '');
-  } catch (e) {
-    console.error("utf8ToBase64 error", e);
-    return '';
-  }
-}
+var LZString = (function() {
+  var f = String.fromCharCode;
+  var keyStrUriSafe = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+  var baseReverseDic = {};
 
-function base64ToUtf8(str) {
-  try {
-    let base64 = str.trim().replace(/-/g, '+').replace(/_/g, '/');
-    while (base64.length % 4) {
-      base64 += '=';
+  function getBaseValue(alphabet, character) {
+    if (!baseReverseDic[alphabet]) {
+      baseReverseDic[alphabet] = {};
+      for (var i=0 ; i<alphabet.length ; i++) {
+        baseReverseDic[alphabet][alphabet.charAt(i)] = i;
+      }
     }
-    const binary = atob(base64);
-    const bytes = new Uint8Array(binary.length);
-    for (let i = 0; i < binary.length; i++) {
-      bytes[i] = binary.charCodeAt(i);
-    }
-    return new TextDecoder('utf-8').decode(bytes);
-  } catch (e) {
-    console.warn("base64ToUtf8 decode error", e);
-    return null;
+    return baseReverseDic[alphabet][character];
   }
-}
+
+  return {
+    compressToEncodedURIComponent: function (input) {
+      if (input == null) return "";
+      return this._compress(input, 6, function(a){return keyStrUriSafe.charAt(a);});
+    },
+    decompressFromEncodedURIComponent: function (input) {
+      if (input == null) return "";
+      if (input == "") return null;
+      input = input.replace(/ /g, "+");
+      return this._decompress(input.length, 32, function(index){return getBaseValue(keyStrUriSafe, input.charAt(index));});
+    },
+
+    _compress: function (uncompressed, bitsPerChar, getCharFromInt) {
+      if (uncompressed == null) return "";
+      var i, value,
+          context_dictionary= {},
+          context_dictionaryToCreate= {},
+          context_c="",
+          context_wc="",
+          context_w="",
+          context_enlargeIn= 2,
+          context_dictSize= 3,
+          context_numBits= 2,
+          context_data=[],
+          context_data_val=0,
+          context_data_position=0,
+          ii;
+
+      for (ii = 0; ii < uncompressed.length; ii += 1) {
+        context_c = uncompressed.charAt(ii);
+        if (!Object.prototype.hasOwnProperty.call(context_dictionary,context_c)) {
+          context_dictionary[context_c] = context_dictSize++;
+          context_dictionaryToCreate[context_c] = true;
+        }
+
+        context_wc = context_w + context_c;
+        if (Object.prototype.hasOwnProperty.call(context_dictionary,context_wc)) {
+          context_w = context_wc;
+        } else {
+          if (Object.prototype.hasOwnProperty.call(context_dictionaryToCreate,context_w)) {
+            if (context_w.charCodeAt(0)<256) {
+              for (i=0 ; i<context_numBits ; i++) {
+                context_data_val = (context_data_val << 1);
+                if (context_data_position == bitsPerChar - 1) {
+                  context_data_position = 0;
+                  context_data.push(getCharFromInt(context_data_val));
+                  context_data_val = 0;
+                } else {
+                  context_data_position++;
+                }
+              }
+              value = context_w.charCodeAt(0);
+              for (i=0 ; i<8 ; i++) {
+                context_data_val = (context_data_val << 1) | (value & 1);
+                if (context_data_position == bitsPerChar - 1) {
+                  context_data_position = 0;
+                  context_data.push(getCharFromInt(context_data_val));
+                  context_data_val = 0;
+                } else {
+                  context_data_position++;
+                }
+                value = value >> 1;
+              }
+            } else {
+              value = 1;
+              for (i=0 ; i<context_numBits ; i++) {
+                context_data_val = (context_data_val << 1) | value;
+                if (context_data_position == bitsPerChar - 1) {
+                  context_data_position = 0;
+                  context_data.push(getCharFromInt(context_data_val));
+                  context_data_val = 0;
+                } else {
+                  context_data_position++;
+                }
+                value = 0;
+              }
+              value = context_w.charCodeAt(0);
+              for (i=0 ; i<16 ; i++) {
+                context_data_val = (context_data_val << 1) | (value & 1);
+                if (context_data_position == bitsPerChar - 1) {
+                  context_data_position = 0;
+                  context_data.push(getCharFromInt(context_data_val));
+                  context_data_val = 0;
+                } else {
+                  context_data_position++;
+                }
+                value = value >> 1;
+              }
+            }
+            context_enlargeIn--;
+            if (context_enlargeIn == 0) {
+              context_enlargeIn = Math.pow(2, context_numBits);
+              context_numBits++;
+            }
+            delete context_dictionaryToCreate[context_w];
+          } else {
+            value = context_dictionary[context_w];
+            for (i=0 ; i<context_numBits ; i++) {
+              context_data_val = (context_data_val << 1) | (value & 1);
+              if (context_data_position == bitsPerChar - 1) {
+                context_data_position = 0;
+                context_data.push(getCharFromInt(context_data_val));
+                context_data_val = 0;
+              } else {
+                context_data_position++;
+              }
+              value = value >> 1;
+            }
+          }
+          context_enlargeIn--;
+          if (context_enlargeIn == 0) {
+            context_enlargeIn = Math.pow(2, context_numBits);
+            context_numBits++;
+          }
+          context_dictionary[context_wc] = context_dictSize++;
+          context_w = String(context_c);
+        }
+      }
+
+      if (context_w !== "") {
+        if (Object.prototype.hasOwnProperty.call(context_dictionaryToCreate,context_w)) {
+          if (context_w.charCodeAt(0)<256) {
+            for (i=0 ; i<context_numBits ; i++) {
+              context_data_val = (context_data_val << 1);
+              if (context_data_position == bitsPerChar - 1) {
+                context_data_position = 0;
+                context_data.push(getCharFromInt(context_data_val));
+                context_data_val = 0;
+              } else {
+                context_data_position++;
+              }
+            }
+            value = context_w.charCodeAt(0);
+            for (i=0 ; i<8 ; i++) {
+              context_data_val = (context_data_val << 1) | (value & 1);
+              if (context_data_position == bitsPerChar - 1) {
+                context_data_position = 0;
+                context_data.push(getCharFromInt(context_data_val));
+                context_data_val = 0;
+              } else {
+                context_data_position++;
+              }
+              value = value >> 1;
+            }
+          } else {
+            value = 1;
+            for (i=0 ; i<context_numBits ; i++) {
+              context_data_val = (context_data_val << 1) | value;
+              if (context_data_position == bitsPerChar - 1) {
+                context_data_position = 0;
+                context_data.push(getCharFromInt(context_data_val));
+                context_data_val = 0;
+              } else {
+                context_data_position++;
+              }
+              value = 0;
+            }
+            value = context_w.charCodeAt(0);
+            for (i=0 ; i<16 ; i++) {
+              context_data_val = (context_data_val << 1) | (value & 1);
+              if (context_data_position == bitsPerChar - 1) {
+                context_data_position = 0;
+                context_data.push(getCharFromInt(context_data_val));
+                context_data_val = 0;
+              } else {
+                context_data_position++;
+              }
+              value = value >> 1;
+            }
+          }
+          context_enlargeIn--;
+          if (context_enlargeIn == 0) {
+            context_enlargeIn = Math.pow(2, context_numBits);
+            context_numBits++;
+          }
+          delete context_dictionaryToCreate[context_w];
+        } else {
+          value = context_dictionary[context_w];
+          for (i=0 ; i<context_numBits ; i++) {
+            context_data_val = (context_data_val << 1) | (value & 1);
+            if (context_data_position == bitsPerChar - 1) {
+              context_data_position = 0;
+              context_data.push(getCharFromInt(context_data_val));
+              context_data_val = 0;
+            } else {
+              context_data_position++;
+            }
+            value = value >> 1;
+          }
+        }
+        context_enlargeIn--;
+        if (context_enlargeIn == 0) {
+          context_enlargeIn = Math.pow(2, context_numBits);
+          context_numBits++;
+        }
+      }
+
+      value = 2;
+      for (i=0 ; i<context_numBits ; i++) {
+        context_data_val = (context_data_val << 1) | (value & 1);
+        if (context_data_position == bitsPerChar - 1) {
+          context_data_position = 0;
+          context_data.push(getCharFromInt(context_data_val));
+          context_data_val = 0;
+        } else {
+          context_data_position++;
+        }
+        value = value >> 1;
+      }
+
+      while (true) {
+        context_data_val = (context_data_val << 1);
+        if (context_data_position == bitsPerChar - 1) {
+          context_data.push(getCharFromInt(context_data_val));
+          break;
+        } else context_data_position++;
+      }
+      return context_data.join('');
+    },
+
+    _decompress: function (length, resetValue, getNextValue) {
+      var dictionary = [],
+          next,
+          enlargeIn = 4,
+          dictSize = 4,
+          numBits = 3,
+          entry = "",
+          result = [],
+          i,
+          w,
+          bits, resb, maxpower, power,
+          c,
+          data = {val:getNextValue(0), position:resetValue, index:1};
+
+      for (i = 0; i < 3; i += 1) {
+        dictionary[i] = i;
+      }
+
+      bits = 0;
+      maxpower = Math.pow(2,2);
+      power=1;
+      while (power!=maxpower) {
+        resb = data.val & data.position;
+        data.position >>= 1;
+        if (data.position == 0) {
+          data.position = resetValue;
+          data.val = getNextValue(data.index++);
+        }
+        bits |= (resb>0?1:0) * power;
+        power <<= 1;
+      }
+
+      switch (next = bits) {
+        case 0:
+            bits = 0;
+            maxpower = Math.pow(2,8);
+            power=1;
+            while (power!=maxpower) {
+              resb = data.val & data.position;
+              data.position >>= 1;
+              if (data.position == 0) {
+                data.position = resetValue;
+                data.val = getNextValue(data.index++);
+              }
+              bits |= (resb>0?1:0) * power;
+              power <<= 1;
+            }
+            c = f(bits);
+            break;
+        case 1:
+            bits = 0;
+            maxpower = Math.pow(2,16);
+            power=1;
+            while (power!=maxpower) {
+              resb = data.val & data.position;
+              data.position >>= 1;
+              if (data.position == 0) {
+                data.position = resetValue;
+                data.val = getNextValue(data.index++);
+              }
+              bits |= (resb>0?1:0) * power;
+              power <<= 1;
+            }
+            c = f(bits);
+            break;
+        case 2:
+            return "";
+      }
+      dictionary[3] = c;
+      w = c;
+      result.push(c);
+      while (true) {
+        if (data.index > length) {
+          return "";
+        }
+
+        bits = 0;
+        maxpower = Math.pow(2,numBits);
+        power=1;
+        while (power!=maxpower) {
+          resb = data.val & data.position;
+          data.position >>= 1;
+          if (data.position == 0) {
+            data.position = resetValue;
+            data.val = getNextValue(data.index++);
+          }
+          bits |= (resb>0?1:0) * power;
+          power <<= 1;
+        }
+
+        switch (c = bits) {
+          case 0:
+              bits = 0;
+              maxpower = Math.pow(2,8);
+              power=1;
+              while (power!=maxpower) {
+                resb = data.val & data.position;
+                data.position >>= 1;
+                if (data.position == 0) {
+                  data.position = resetValue;
+                  data.val = getNextValue(data.index++);
+                }
+                bits |= (resb>0?1:0) * power;
+                power <<= 1;
+              }
+
+              dictionary[dictSize++] = f(bits);
+              c = dictSize-1;
+              enlargeIn--;
+              break;
+          case 1:
+              bits = 0;
+              maxpower = Math.pow(2,16);
+              power=1;
+              while (power!=maxpower) {
+                resb = data.val & data.position;
+                data.position >>= 1;
+                if (data.position == 0) {
+                  data.position = resetValue;
+                  data.val = getNextValue(data.index++);
+                }
+                bits |= (resb>0?1:0) * power;
+                power <<= 1;
+              }
+
+              dictionary[dictSize++] = f(bits);
+              c = dictSize-1;
+              enlargeIn--;
+              break;
+          case 2:
+              return result.join("");
+        }
+
+        if (enlargeIn == 0) {
+          enlargeIn = Math.pow(2, numBits);
+          numBits++;
+        }
+
+        if (dictionary[c]) {
+          entry = dictionary[c];
+        } else {
+          if (c === dictSize) {
+            entry = w + w.charAt(0);
+          } else {
+            return null;
+          }
+        }
+        result.push(entry);
+
+        dictionary[dictSize++] = w + entry.charAt(0);
+        enlargeIn--;
+
+        if (enlargeIn == 0) {
+          enlargeIn = Math.pow(2, numBits);
+          numBits++;
+        }
+
+        w = entry;
+      }
+    }
+  };
+})();
 
 export function compressTestToUrlToken(payload) {
   if (!payload || !Array.isArray(payload.questions)) return '';
@@ -46,7 +409,7 @@ export function compressTestToUrlToken(payload) {
     .replace(/<[^>]+>/g, ' ')
     .replace(/\s+/g, ' ')
     .trim()
-    .substring(0, 200);
+    .substring(0, 150);
 
   const minified = {
     t: (payload.title || 'Test BUS').substring(0, 70),
@@ -55,18 +418,23 @@ export function compressTestToUrlToken(payload) {
       q.question,
       q.options,
       q.correctAnswer,
-      (q.explanation || '').substring(0, 120)
+      (q.explanation || '').substring(0, 100)
     ])
   };
 
-  const jsonStr = JSON.stringify(minified);
-  return utf8ToBase64(jsonStr);
+  try {
+    const jsonStr = JSON.stringify(minified);
+    return LZString.compressToEncodedURIComponent(jsonStr);
+  } catch (err) {
+    console.error("Error compressing LZ test payload:", err);
+    return '';
+  }
 }
 
 export function decompressUrlTokenToTest(token) {
   if (!token || typeof token !== 'string') return null;
 
-  // Si el token aún trae secuencias %7B (versión antigua), fallback a decodeURIComponent(atob)
+  // Fallback si trae secuencias antiguas base64 / %7B
   if (token.includes('%7B') || token.includes('%22')) {
     try {
       const decodedStr = decodeURIComponent(token.trim());
@@ -87,11 +455,11 @@ export function decompressUrlTokenToTest(token) {
     } catch (_) {}
   }
 
-  const jsonStr = base64ToUtf8(token);
-  if (!jsonStr) return null;
-
   try {
-    const minified = JSON.parse(jsonStr);
+    const decompressedJson = LZString.decompressFromEncodedURIComponent(token.trim());
+    if (!decompressedJson) return null;
+
+    const minified = JSON.parse(decompressedJson);
     if (!minified || !Array.isArray(minified.q)) return null;
 
     return {
@@ -106,7 +474,7 @@ export function decompressUrlTokenToTest(token) {
       }))
     };
   } catch (err) {
-    console.warn("Could not parse decompressed test JSON", err);
+    console.warn("Could not parse decompressed LZ test JSON", err);
     return null;
   }
 }
